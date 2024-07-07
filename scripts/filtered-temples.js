4508b0922e1169cfc9013faa76847250c6e61817
adonaivillalobos <vil21022@byui.edu>
2024-07-07 05:12:29 +0200

    changes

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -606,11 +606,3 @@ document.getElementById("temple-select").addEventListener("change", function() {
   const selectedTemple = temples[this.value];
   displayTempleInfo(selectedTemple);
 });
-
-// Initialize the temple select dropdown
-for (let i = 0; i < temples.length; i++) {
-  const templeOption = document.createElement("option");
-  templeOption.value = i;
-  templeOption.text = temples[i].templeName;
-  document.getElementById("temple-select").appendChild(templeOption);
-}

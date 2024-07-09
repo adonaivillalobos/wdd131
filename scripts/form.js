45a494bd9fc827a692ebd1ae59dffcbd75afebaf
adonaivillalobos <vil21022@byui.edu>
2024-07-10 00:18:36 +0200

    js

--- scripts/form.js
+++ scripts/form.js
@@ -3,7 +3,9 @@ const products = [
     { name: 'Product 1' },
     { name: 'Product 2' },
     { name: 'Product 3' },
-    { name: 'Product 4' }
+    { name: 'Product 4' },
+    { name: 'Product 5' },
+    { name: 'Product 6' }
 ];
 
 const productSelect = document.getElementById('product');
